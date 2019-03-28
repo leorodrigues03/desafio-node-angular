d83b411a0e9a961091ac311dbc56b0d8944b4725
Leo Rodrigues <leonardo_rsandrade@hotmail.com>
2019-03-28 16:56:58 -0300

    Criar metodos GET

--- api/controllers/curso.controller.js
+++ api/controllers/curso.controller.js
@@ -1,6 +1,7 @@
 const Curso = require('../models/curso.model.js');
 
-exports.create = (req,res) => {
+
+exports.create = (req, res) => {
     
     //Validation
     if (!req.body) {
@@ -26,4 +27,47 @@ exports.create = (req,res) => {
             message: err.message || "Erro ao criar o curso!"
         });
     });
+};
+
+exports.readAll = (req, res) => {
+    Curso.find()
+    .then(cursos => {
+        res.send(cursos);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Erro ao buscar cursos!"
+        });
+    });
+
+};
+
+exports.readById = (req, res) => {
+    var id = req.params.cursoId;
+    Curso.findById(id)
+    .then(curso => {
+        if (!curso) {
+            return res.status(404).send({
+                message: "Curso '" + id + "' não encontrado."
+            });
+        }
+        res.send(curso);
+    }).catch (err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Curso '" + id + "' não encontrado."
+            });
+        }
+
+        return res.status(500).send({
+            message: "Erro ao buscar Curso Id: " + id
+        });
+    });
+};
+
+exports.update = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+
 };
--- api/routes/curso.routes.js
+++ api/routes/curso.routes.js
@@ -2,6 +2,11 @@ module.exports = (app) => {
     const curso = require('../controllers/curso.controller.js');
 
     //POST
-    app.post('/cursos',curso.create);
+    app.post('/cursos', curso.create);
 
+    //GET All
+    app.get('/cursos', curso.readAll);
+
+    //GET ById
+    app.get('/cursos/:cursoId', curso.readById);
 }
