ee1fd3732661a9069c622485989ec218d802c86a
Leo Rodrigues <leonardo_rsandrade@hotmail.com>
2019-03-28 23:58:37 -0300

    correcao da api (enable cors)

--- api/server.js
+++ api/server.js
@@ -8,6 +8,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//Enable CORS for all HTTP methods
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 //Routes
 require('./routes/curso.routes.js')(app);
 
